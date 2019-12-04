1c0a96b86380167a787b3ab333254d707cf34a1b
Anton Korepanov <tony1000@ya.ru>
2019-12-04 15:55:05 +0500

    add react-ui-tree and format data to build it in sidebar

--- test-web-viewer/test-web-viewer/ClientApp/src/components/WebViewer.js
+++ test-web-viewer/test-web-viewer/ClientApp/src/components/WebViewer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './WebViewer.css';
 import SplitPane from 'react-split-pane';
+import Tree from 'react-ui-tree';
 
 export class WebViewer extends Component {
 
@@ -8,7 +9,11 @@ export class WebViewer extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { direction: "vertical" };
+        this.state = {
+            tree: { module: "img", children: [] },
+            directories: {},
+            direction: "vertical"
+        };
     }
 
     updateDimensions() {
@@ -23,6 +28,7 @@ export class WebViewer extends Component {
     };
 
     componentDidMount() {
+        this.populateDirectoriesData();
         window.addEventListener('resize', this.updateDimensions());
     }
 
@@ -30,13 +36,28 @@ export class WebViewer extends Component {
         window.removeEventListener('resize', this.updateDimensions());
     }
 
+    renderNode = node => {
+        return (
+            <span className="folderName">
+                {node.module}
+            </span>
+        );
+    };
+
+
     render() {
         return (
             <SplitPane
                 split={this.state.direction}
                 defaultSize={200} primary="first"
             >
-                <div id="sidebar"></div>
+                <div id="sidebar">
+                   <Tree
+                        paddingLeft={20}              // left padding for children nodes in pixels
+                        tree={this.state.tree}        // tree object
+                        renderNode={this.renderNode}  // renderNode(node) return react element
+                    />
+                </div>
                 <div id="viewer">
                     <SplitPane
                         split="horizontal"
@@ -48,5 +69,62 @@ export class WebViewer extends Component {
                 </div>
             </SplitPane>
         );
+    }
+
+    convertToTree(data) {
+        // data
+        //{
+        //    "directoryName1": {
+        //        "path": "directory1FullPath",
+        //        "children": {
+        //            "directoryName2": { ... }
+        //            "directoryName3": { ... }
+        //        }
+        //    }
+        //}
+
+        // result
+        //{
+        //    "module": "directoryName1",
+        //    "path": 
+        //    "children": [{
+        //            "module": "directoryName2",
+        //            "children": [{
+        //                "module": "..."
+        //            }]
+        //        },
+        //        {
+        //            "module": "directoryName2",
+        //            "children": [{
+        //                "module": "..."
+        //            }]
+        //        }
+        //    ]
+        //}
+        let tree = [];
+        for (let property in data) {
+            if (data.hasOwnProperty(property)) {
+                let node = {
+                    module: property,
+                    path: data[property].path
+                };
+                if (data[property].children) {
+                    node.children = this.convertToTree(data[property].children);
+                }
+                tree.push(node);
+            }
+        }
+        return tree;
+    }
+
+    async populateDirectoriesData() {
+        const response = await fetch('api/getdirectoriestree');
+        const data = await response.json();
+        console.log(data);
+        const children = this.convertToTree(data);
+        console.log(children);
+        let tree = this.state.tree;
+        tree.children = children;
+        this.setState({ tree: tree, directories: data });
     }
 }
