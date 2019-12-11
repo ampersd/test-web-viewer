84053b51ed003d9b76d2d1ee1cec105ca4f1e6f2
Anton Korepanov <tony1000@ya.ru>
2019-12-11 20:20:57 +0500

    remove buggy "react-ui-tree", use "rc-tree" instead, create separate Tree compoment

---
+++ test-web-viewer/test-web-viewer/ClientApp/src/components/Tree.jsx
@@ -0,0 +1,160 @@
+﻿import React, { Component } from "react";
+import RCTree, { TreeNode } from "rc-tree";
+
+import "rc-tree/assets/index.css";
+import "./Tree.scss";
+
+export class Tree extends Component {
+  state = {
+    tree: undefined,
+    expanded: []
+  };
+
+  componentDidMount() {
+    // загружаем данные дерева
+    this.populateDirectoriesData();
+  }
+
+  onExpand = (...args) => {
+    console.log("onExpand", ...args);
+  };
+
+  onCheck = (checkedKeys, info) => {
+    console.log("onCheck", checkedKeys, info);
+  };
+
+  switcherIcon = obj => {
+    if (obj.isLeaf === true) return "";
+    if (obj.expanded === true) return "▽";
+    return "▷";
+  };
+
+    convertToTree(data, parentKeyStr) {
+        // data
+        //{
+        //    "directoryName1": {
+        //        "path": "directory1FullPath",
+        //        "children": {
+        //            "directoryName2": { ... }
+        //            "directoryName3": { ... }
+        //        }
+        //    }
+        //}
+
+        //[
+        //    {
+        //        key: "0-0",
+        //        title: "parent 1",
+        //        children: [
+        //            {
+        //                key: "0-0-0",
+        //                title: "parent 1-1",
+        //                children: [{ key: "0-0-0-0", title: "parent 1-1-0" }]
+        //            },
+        //            {
+        //                key: "0-0-1",
+        //                title: "parent 1-2",
+        //                children: [
+        //                    {
+        //                        key: "0-0-1-0",
+        //                        title: "parent 1-2-0"
+        //                    },
+        //                    { key: "0-0-1-1", title: "parent 1-2-1" }
+        //                ]
+        //            }
+        //        ]
+        //    }
+        //]
+    if (parentKeyStr === undefined) parentKeyStr = "";
+    let tree = [];
+    let index = 0;
+    for (let directoryNameProperty in data) {
+      if (data.hasOwnProperty(directoryNameProperty)) {
+        let key = parentKeyStr + "-" + index;
+        let node = {
+          title: directoryNameProperty,
+          path: data[directoryNameProperty].path,
+          key: key
+        };
+        if (data[directoryNameProperty].children) {
+          node.children = this.convertToTree(
+            data[directoryNameProperty].children,
+            key
+          );
+        }
+        tree.push(node);
+        index++;
+      }
+    }
+    return tree;
+  }
+
+  async populateDirectoriesData() {
+    const response = await fetch(
+      "api/getdirectoriestree"
+    );
+    const data = await response.json();
+    console.log(data);
+    // тут преобразуем в тот формат который тебе нужен
+    const children = this.convertToTree(data);
+    // ставим в стейт
+    this.setState({ tree: children });
+  }
+
+  buildTree = data => {
+    return data.map(item => {
+        if (item.children && item.children.length > 0) {
+        // console.log(item.children);
+        return (
+          <TreeNode
+            title={item.title}
+            key={item.key}
+            // className="node"
+            style={{
+              fontWeight: "bold",
+              color: "#a9a9a9"
+            }}
+          >
+            {this.buildTree(item.children)}
+          </TreeNode>
+        );
+      }
+      console.log(item);
+      return (
+        <TreeNode
+          title={item.title}
+          key={item.key}
+          isLeaf={item.isLeaf}
+          disabled={item.key === "0-0-0"}
+          style={{ fontWeight: "normal" }}
+        />
+      );
+    });
+  };
+
+  render() {
+    const { tree } = this.state;
+
+    // если дерева пока нет, ничего не рендерим
+    if (!tree) {
+      return null;
+    }
+
+    // если есть строим дерево и рендерим его
+
+    const treeNodes = this.buildTree(tree);
+
+    return (
+      <RCTree
+        defaultExpandAll
+        onExpand={this.onExpand}
+        onSelect={this.onSelect}
+        onCheck={this.onCheck}
+        switcherIcon={this.switcherIcon}
+        showIcon={false}
+      >
+        {treeNodes}
+      </RCTree>
+    );
+  }
+}
--- test-web-viewer/test-web-viewer/ClientApp/src/components/WebViewer.js
+++ test-web-viewer/test-web-viewer/ClientApp/src/components/WebViewer.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import './WebViewer.css';
-import SplitPane from 'react-split-pane';
-import Tree from 'react-ui-tree';
+﻿import React, { Component } from "react";
+import "./WebViewer.scss";
+import SplitPane from "react-split-pane";
+import { Tree } from "./Tree";
 
 export class WebViewer extends Component {
-
     static displayName = WebViewer.name;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            tree: { module: "img", children: [] },
-            directories: {},
-            direction: "vertical"
-        };
-    }
-
+    state = {
+        direction: "vertical",
+        expanded: []
+    };
     updateDimensions() {
         return () => {
             let width = window.innerWidth;
@@ -24,109 +18,29 @@ export class WebViewer extends Component {
             } else {
                 this.setState({ direction: "vertical" });
             }
-        }
-    };
+        };
+    }
 
     componentDidMount() {
-        this.populateDirectoriesData();
-        window.addEventListener('resize', this.updateDimensions());
+        window.addEventListener("resize", this.updateDimensions());
     }
 
     componentWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions());
+        window.removeEventListener("resize", this.updateDimensions());
     }
-
-    renderNode = node => {
-        return (
-            <span className="folderName"
-                style={(node.children && node.children.length > 0) ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}
-            >
-                {node.module}
-            </span>
-        );
-    };
-
-
     render() {
         return (
-            <SplitPane
-                split={this.state.direction}
-                defaultSize={200} primary="first"
-            >
+            <SplitPane split={this.state.direction} defaultSize={200} primary="first">
                 <div id="sidebar">
-                   <Tree
-                        paddingLeft={20}              // left padding for children nodes in pixels
-                        tree={this.state.tree}        // tree object
-                        renderNode={this.renderNode}  // renderNode(node) return react element
-                    />
+                    <Tree />
                 </div>
                 <div id="viewer">
-                    <SplitPane
-                        split="horizontal"
-                        defaultSize={200} primary="first"
-                    >
-                        <div id="top"></div>
-                        <div id="bottom"></div>
+                    <SplitPane split="horizontal" defaultSize={200} primary="first">
+                        <div id="top" />
+                        <div id="bottom" />
                     </SplitPane>
                 </div>
             </SplitPane>
         );
-    }
-
-    convertToTree(data) {
-        // data
-        //{
-        //    "directoryName1": {
-        //        "path": "directory1FullPath",
-        //        "children": {
-        //            "directoryName2": { ... }
-        //            "directoryName3": { ... }
-        //        }
-        //    }
-        //}
-
-        // result
-        //{
-        //    "module": "directoryName1",
-        //    "path": 
-        //    "children": [{
-        //            "module": "directoryName2",
-        //            "children": [{
-        //                "module": "..."
-        //            }]
-        //        },
-        //        {
-        //            "module": "directoryName2",
-        //            "children": [{
-        //                "module": "..."
-        //            }]
-        //        }
-        //    ]
-        //}
-        let tree = [];
-        for (let property in data) {
-            if (data.hasOwnProperty(property)) {
-                let node = {
-                    module: property,
-                    path: data[property].path
-                };
-                if (data[property].children) {
-                    node.children = this.convertToTree(data[property].children);
-                }
-                tree.push(node);
-            }
-        }
-        return tree;
-    }
-
-    async populateDirectoriesData() {
-        const response = await fetch('api/getdirectoriestree');
-        const data = await response.json();
-        console.log(data);
-        const children = this.convertToTree(data);
-        console.log(children);
-        let tree = this.state.tree;
-        tree.children = children;
-        this.setState({ tree: tree, directories: data });
     }
 }
